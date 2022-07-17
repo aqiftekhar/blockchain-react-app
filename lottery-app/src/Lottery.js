66120a0d566a7ae587a1aec470786f07674040db
aqdasiftekhar <aqdasiftekhar@gmail.com>
2022-07-18 02:36:03 +0500

    created a localy deployed contract using ABI & Address

---
+++ lottery-app/src/Lottery.js
@@ -0,0 +1,66 @@
+import web3 from "./web3";
+
+const address = '0x8f46c39Bd5dc1dd21b07241EddC72d8b1DAb80DD';
+
+const AbiInterface = [
+    {
+      inputs: [],
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+      constant: undefined,
+      payable: undefined,
+      signature: 'constructor'
+    },
+    {
+      inputs: [],
+      name: 'enter',
+      outputs: [],
+      stateMutability: 'payable',
+      type: 'function',
+      constant: undefined,
+      payable: true,
+      signature: '0xe97dcb62'
+    },
+    {
+      inputs: [],
+      name: 'getPlayers',
+      outputs: [ [Object] ],
+      stateMutability: 'view',
+      type: 'function',
+      constant: true,
+      payable: undefined,
+      signature: '0x8b5b9ccc'
+    },
+    {
+      inputs: [],
+      name: 'manager',
+      outputs: [ [Object] ],
+      stateMutability: 'view',
+      type: 'function',
+      constant: true,
+      payable: undefined,
+      signature: '0x481c6a75'
+    },
+    {
+      inputs: [],
+      name: 'pickWinner',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+      constant: undefined,
+      payable: undefined,
+      signature: '0x5d495aea'
+    },
+    {
+      inputs: [ [Object] ],
+      name: 'players',
+      outputs: [ [Object] ],
+      stateMutability: 'view',
+      type: 'function',
+      constant: true,
+      payable: undefined,
+      signature: '0xf71d96cb'
+    }
+  ];
+
+  export default new web3.eth.Contract(AbiInterface, address);
